3548c8fb12ea1e4cd25126d96e2decead395b387
Kyhong <kyhong222@naver.com>
2021-09-28 17:20:02 +0900

    added rune display component

--- src/components/RuneDisplayer.js
+++ src/components/RuneDisplayer.js
@@ -1,15 +1,79 @@
 import * as React from "react";
-import { Table } from "@mui/material";
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableContainer,
+  TableRow,
+  Paper,
+} from "@mui/material";
 import { Link } from "react-router-dom";
 
 export default function RuneDisplayer(props) {
   const rune = props.rune; // number, engName, korName, specWithWeapon, specWithHelmNChest, reqLvl, reqToUp, note
-  // console.log("props", props)
-  // console.log("rune props", rune)
+  console.log("rune", rune);
+  const tableRowSx = { "&:last-child td, &:last-child th": { border: 0 } };
+
   return (
     <div>
-      rune display
-      {}
+      <TableContainer component={Paper}>
+        <Table sx={{ minWidth: 200 }} aria-label="rune display table">
+          <TableBody>
+            <TableRow sx={{ tableRowSx }}>
+              <TableCell align="center" colSpan={1}>
+                {"image"}
+              </TableCell>
+              <TableCell align="center" colSpan={4}>
+                {`[${rune.number}] ${rune.korName} / ${rune.engName}`}
+              </TableCell>
+            </TableRow>
+            <TableRow sx={{ tableRowSx }}>
+              <TableCell align="center" colSpan={1}>
+                레벨 제한
+              </TableCell>
+              <TableCell align="left" colSpan={4}>
+                {`${rune.reqLvl} 레벨 이상`}
+              </TableCell>
+            </TableRow>
+            <TableRow sx={{ tableRowSx }}>
+              <TableCell align="center" colSpan={1}>
+                무기 장착시
+              </TableCell>
+              <TableCell align="left" colSpan={4}>
+                {rune.specWithWeapon}
+              </TableCell>
+            </TableRow>
+            <TableRow sx={{ tableRowSx }}>
+              <TableCell align="center" colSpan={1}>
+                투구/갑옷 장착시
+              </TableCell>
+              <TableCell align="left" colSpan={4}>
+                {rune.specWithHelmNChest}
+              </TableCell>
+            </TableRow>
+            <TableRow sx={{ tableRowSx }}>
+              <TableCell align="center" colSpan={1}>
+                방패 장착시
+              </TableCell>
+              <TableCell align="left" colSpan={4}>
+                {rune.specWithShield}
+              </TableCell>
+            </TableRow>
+            {rune.note ? (
+              <TableRow sx={{ tableRowSx }}>
+                <TableCell align="center" colSpan={1}>
+                  비고
+                </TableCell>
+                <TableCell align="left" colSpan={4}>
+                  {rune.note}
+                </TableCell>
+              </TableRow>
+            ) : (
+              <></>
+            )}
+          </TableBody>
+        </Table>
+      </TableContainer>
     </div>
   );
 }
